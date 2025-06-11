aec885e5925046b1ebd3e8871e58ddffb97074db
rami <ramialzend@gmail.com>
2025-06-11 06:03:45 +0300

    nothing

--- TMS.Server/Program.cs
+++ TMS.Server/Program.cs
@@ -1,15 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers().AddNewtonsoftJson(options =>
 {
     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
 });
+
 builder.Services.AddOpenApi();
+
 builder.Services.AddHostedService<DatabaseSyncService>();
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 ArgumentNullException.ThrowIfNull(connectionString);
 builder.Services.AddAppDependencyInjection(connectionString, builder.Configuration);
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -17,13 +20,13 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
 await app.InitializeDatabaseAsync();
 
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.MapControllers();
 
