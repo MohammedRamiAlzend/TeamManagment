a2cc54bd2ca1db9a32a95e62a9ca0d2a548f810a
rami <ramialzend@gmail.com>
2025-06-14 04:08:22 +0300

    remove unneccessary project

--- Contracts/GlobalUsing.cs
+++
@@ -1,7 +0,0 @@
-global using System.Linq.Expressions;
-global using Microsoft.EntityFrameworkCore.Query;
-global using TMS.Core;
-global using TMS.Core.Entities.Interfaces;
-global using TMS.Core.MediatR.Interfaces;
-global using TMS.Core.Entities.Models;
-global using TMS.Core.Entities;
